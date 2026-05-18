097b6936685d42a564e273438fb6cab2d43d0c3e
UnknownCreator27 <steel27123@gmail.com>
2026-05-18 18:04:47 +0200

    Fixed forms

--- Forms/Form1.cs
+++ Forms/Form1.cs
@@ -22,10 +22,7 @@ namespace PRG262_Bob_s_Gym
         public Form1()
         {
             InitializeComponent();
-            
         }
-
-
 
         private void DOBPicker_ValueChanged(object sender, EventArgs e)
         {
